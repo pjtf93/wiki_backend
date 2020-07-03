f761cb3e0c641d4f55f8d4eca7dbb99dbe6617cf
Pablo Tovar <pjtf93@gmail.com>
2020-07-03 00:46:18 -0400

    Deleting old documents

--- index.js
+++ index.js
@@ -28,39 +28,3 @@ sequelize.sync().then(() => {
     console.log(`Example app listening on port ${process.env.PORT}!`);
   });
 });
-
-/* const express = require('express');
-const app = express();
-
-const { config } = require('./wiki_api/config/index');
-const wikiApi = require('./wiki_api/routes/wiki');
-
-const {
-  logErrors,
-  wrapErrors,
-  errorHandler,
-} = require('./wiki_api/utils/middleware/errorHandlers.js');
-
-const notFoundHandler = require('./wiki_api/utils/middleware/notFoundHandler');
-// const port = 3001;
-// const { config } = require('./config/index');
-// app.get('/', (req, res) => res.send('Hello World!'));
-
-// body parser
-app.use(express.json());
-
-// routes
-wikiApi(app);
-
-// Catch 404
-app.use(notFoundHandler);
-
-// Errores middleware
-app.use(logErrors);
-app.use(wrapErrors);
-app.use(errorHandler);
-
-app.listen(config.port, () =>
-  console.log(`Example app listening at http://localhost:${config.port}`)
-);
- */
